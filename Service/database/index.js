4710cc991582ee61fc4e38e4735e6a8377d7ebbf
Alec Leong <leongalec@gmail.com>
2020-06-03 11:47:17 -0700

    set Mongoose connection option useCreateIndex to true

--- Service/database/index.js
+++ Service/database/index.js
@@ -7,6 +7,7 @@ const SERVER = '127.0.0.1:27017';
 const DB = 'airbnb';
 const URI = `mongodb://${SERVER}/${DB}`;
 const OPTIONS = {
+  useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
