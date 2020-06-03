ec8f3678aabad2f33705ed9df523544cef6aa974
Alec Leong <leongalec@gmail.com>
2020-06-03 05:02:39 -0700

    Mongoose connection

---
+++ Service/database/index.js
@@ -0,0 +1,31 @@
+const colors = require('colors');
+const Promise = require('bluebird');
+const mongoose = Promise.promisifyAll(require('mongoose'));
+
+// conn configuration
+const SERVER = '127.0.0.1:27017';
+const DB = 'airbnb';
+const URI = `mongodb://${SERVER}/${DB}`;
+const OPTIONS = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+};
+
+// initial conn and handle initial conn errors
+mongoose.connect(URI, OPTIONS)
+  .then(() => console.log(`Connected to ${colors.green('MongoDB')}`))
+  .catch(console.error);
+
+// conn to database
+const db = mongoose.connection;
+
+// handle errors after initial conn was established by listening for error events on the conn
+db.on('error', (err) => console.error(err));
+
+// successful conn
+db.once('open', () => {
+  console.log(`Using database ${db.name.green}`);
+});
+
+module.exports.db;
